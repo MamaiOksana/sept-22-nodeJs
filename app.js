491286569ad98a8b1dd84bc83980be25da501be7
MamaiOksana <jayko.mamayko@gmail.com>
2023-03-02 21:13:43 +0200

    add 1 lesson

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-///
+const event = require ('node:events');
+
+const eventEmitter = new event();
+
+eventEmitter.on('click', ()=>{
+    console.log('Click, click');
+});
+
+eventEmitter.emit('click')
