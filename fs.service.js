1b34393bcb678c56ce4dc8a30e2150d0a4a4a16a
MamaiOksana <jayko.mamayko@gmail.com>
2023-03-11 12:52:15 +0200

    Merge remote-tracking branch 'origin/lesson2' into lesson2

--- fs.service.js
+++ fs.service.js
@@ -3,7 +3,7 @@ const fs = require('node:fs/promises');
 const path = require('node:path');
 
 //const dbPath =
-
+//
    const reader =  async ()=>{
         const buffer =  await fs.readFile(path.join(process.cwd(), 'dataBase', 'users.json'));
           const data = buffer.toString();
